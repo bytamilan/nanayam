1f2c3e9f4f6dd6e05b35d2e43fa86840979a26dd
Ajith Berlin A <ajithberlin09@gmail.com>
2026-05-05 17:37:32 +0800

    refactor: Remove login functionality and related components

--- apps/operator-console/src/app/layout.tsx
+++ apps/operator-console/src/app/layout.tsx
@@ -1,12 +1,8 @@
-'use client';
-import { IdentityProvider } from '@/components/IdentityLogin';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
         <body>
-        <IdentityProvider>
             {children}
-        </IdentityProvider>
         </body>
         </html>
     );
